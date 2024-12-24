363975df5a1538a721a07bfd71961fa9c0748d83
beispresheva <beis.presheva@icloud.com>
2024-12-24 13:48:37 +0100

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by KEJES beis on 24.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
