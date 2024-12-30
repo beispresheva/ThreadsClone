39740202e4e0cd7735ac0181fcf2fd9e6a37cd23
beispresheva <beis.presheva@icloud.com>
2024-12-30 13:29:13 +0100

    Init, Login edhe Register Views init

---
+++ ThreadsClone/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  ThreadsClone
+//
+//  Created by KEJES beis on 24.12.24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
---
+++ ThreadsClone/Utils/ViewModifiers/ThreadTextFieldModifier.swift
@@ -0,0 +1,8 @@
+//
+//  ThreadTextFieldModifier.swift
+//  ThreadsClone
+//
+//  Created by KEJES beis on 24.12.24.
+//
+
+import Foundation
